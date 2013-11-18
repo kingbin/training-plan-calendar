d7cc282366b366dadae210fbe90419059bd7360c
Chris Blazek <chris.blazek@gmail.com>
2013-11-18 09:15:13 -0600

    commented out redis conf fornow

--- test.js
+++ test.js
@@ -4,14 +4,14 @@ var express = require("express")
     , stylus = require('stylus')
     , nib = require('nib')
 
-if (process.env.REDISTOGO_URL) {
-    var rtg   = require("url").parse(process.env.REDISTOGO_URL);
-    var redis = require("redis").createClient(rtg.port, rtg.hostname);
-
-    redis.auth(rtg.auth.split(":")[1]);
-} else {
-    var redis = require("redis").createClient();
-}
+//if (process.env.REDISTOGO_URL) {
+//    var rtg   = require("url").parse(process.env.REDISTOGO_URL);
+//    var redis = require("redis").createClient(rtg.port, rtg.hostname);
+//
+//    redis.auth(rtg.auth.split(":")[1]);
+//} else {
+//    var redis = require("redis").createClient();
+//}
 
 
 var app = express();
