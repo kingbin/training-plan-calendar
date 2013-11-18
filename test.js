5b8d6ae6aa8bcad8ae34dd5cbfd49ed4a6afe5c8
Chris Blazek <chris.blazek@gmail.com>
2013-11-17 22:50:17 -0600

    little content/style and a form

--- test.js
+++ test.js
@@ -1,10 +1,49 @@
-var express = require("express");
-var app = express();
-app.use(express.logger());
+var express = require("express")
+    , stylus = require('stylus')
+    , nib = require('nib')
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
+
+
+
+var app = express();
+
+
+
+function compile(str, path) {
+      return stylus(str)
+              .set('filename', path)
+                  .use(nib());
+};
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(express.logger());
+app.use(stylus.middleware(
+              { src: __dirname + '/public'
+                    , compile: compile
+      }
+      ));
+
+app.use(express.static(__dirname + '/public'));
+
+
+
+//app.get('/', function(request, response) {
+//  response.send('Hello World!');
+//});
+
+app.get('/', function (req, res) {
+  res.render('index',
+  { title : 'Home' }
+  )
 });
+
+
+app.post("/upload", function (request, response) {
+    console.log("uploaded file", JSON.stringify(request.files) );
+    response.end("upload complete");
+});
+
+
 
 var port = process.env.PORT || 8081;
 //var port = 8081;
