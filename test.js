1cd089cd12b63a7f2aeb2d93b27f5006104cec06
Chris Blazek <chris.blazek@gmail.com>
2013-11-17 22:01:10 -0600

    bad port

--- test.js
+++ test.js
@@ -6,8 +6,8 @@ app.get('/', function(request, response) {
   response.send('Hello World!');
 });
 
-//var port = process.env.PORT || 8081;
-var port = 8081;
+var port = process.env.PORT || 8081;
+//var port = 8081;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
